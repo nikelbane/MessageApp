a7e08c9933e582a0bfc962c872d95aa923fc5344
Nikhil <nikhilbanerjee@yahoo.com>
2021-08-10 12:39:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/messageapp/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.messageapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button login, register;
+    FirebaseUser firebaseUser;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if(firebaseUser != null){
+            Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.login);
+        register = findViewById(R.id.register);
+
+        login.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, LoginActivity.class)));
+        register.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, RegistrationActivity.class)));
+    }
+}
